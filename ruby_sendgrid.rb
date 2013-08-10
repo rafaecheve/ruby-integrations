cf6c389e19a8d07333deb7dacabe5a93a36b3b89
rafael echeverria <rafael.echverria@gmail.com>
2013-08-10 14:27:20 -0500

    sendgrid example

---
+++ ruby_sendgrid.rb
@@ -0,0 +1,23 @@
+require 'mail'
+Mail.defaults do
+  delivery_method :smtp, { :address   => "smtp.sendgrid.net",
+                           :port      => 587,
+                           :domain    => "rafaecheve.com",
+                           :user_name => "rafaecheve",
+                           :password  => "",
+                           :authentication => 'plain',
+                           :enable_starttls_auto => true }
+end
+
+mail = Mail.deliver do
+  to 'r@rafaecheve.com'
+  from 'Jose <rafa@test.com>'
+  subject 'This is the subject of your email'
+  text_part do
+    body 'Hello world in text'
+  end
+  html_part do
+    content_type 'text/html; charset=UTF-8'
+    body '<b>Hello world in HTML</b>'
+  end
+end
